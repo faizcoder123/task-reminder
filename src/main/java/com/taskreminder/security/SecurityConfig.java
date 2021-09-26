97020772e9ebf667e6d5824370f33c3105eb9e18
faizcoder123 <faizhaq241@gmail.com>
2021-09-26 20:00:52 +0530

    Added auth

--- src/main/java/com/taskreminder/security/SecurityConfig.java
+++ src/main/java/com/taskreminder/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.taskreminder.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -22,16 +23,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             throws Exception {
         auth.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());;
     }
-
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/taskReminder/addUser");
+    }
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.httpBasic()
                 .and()
                 .authorizeRequests()
-                .anyRequest()
-                .authenticated().and()
-                .formLogin()
-                .loginPage("/taskReminder/addUser")
-                .permitAll().and().csrf().disable();
+                .anyRequest().authenticated().and().csrf().disable();
+               ;
     }
 }
